eb6308ee88dda2430642cffa076cf9f3e057f6e6
lauric <lauric.gehu@free.fr>
2023-11-26 20:18:59 +0100

    test avec binomial de proba gamma et lambda

--- SIR_stochastique.py
+++ SIR_stochastique.py
@@ -28,7 +28,26 @@ def sir(SIR, params, dt):
 
     return [S-infection,I+infection-recovery,R+recovery]
 
-params = beta, gamma, N
-for dt in range(1, 200, 0.1):
-      SIR = sir(SIR, params, dt)
+def simulate(beta, gamma, I0, N, tempsMax):
+    params = beta, gamma, N
+    #temps = np.arange(1, 100)
+    t = 1
+    SIR = [[N-I0*N], [I0*N], [0]]
+    tend = 500
+    while t < tempsMax:
+        S, I, R = sir([SIR[0][-1], SIR[1][-1], SIR[2][-1]], params, t)
+        SIR[0].append(S)
+        SIR[1].append(I)
+        SIR[2].append(R)
+        t += 1
 
+    temps = np.arange(1, t+1)
+    
+    plt.plot(temps, SIR[0], label='Susceptibles (non infectées)')
+    plt.plot(temps, SIR[1], label='Infectées')
+    plt.plot(temps, SIR[2], label='Rétablies')
+    plt.xlabel('Temps (jours)')
+    plt.ylabel('Population')
+    plt.legend()
+    plt.grid()
+    plt.show() 
